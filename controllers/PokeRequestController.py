9544d6664a0160a3c62b7c1ebb375076dd1cae09
DaroGard <alexdg997@gmail.com>
2025-08-06 13:51:37 -0600

    Add endpoint's plus controllers updated

--- controllers/PokeRequestController.py
+++ controllers/PokeRequestController.py
@@ -9,6 +9,31 @@ from utils.AQueue import AQueue
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+async def select_poke_request( id : int ):
+    try:
+        query = "select * from pokequeue.requests where id = ?"
+        params = (id,)
+        result = await execute_query_json(query, params)
+        result_dict = json.loads(result)
+        return result_dict
+    except Exception as e:
+        logger.error(f"Error selecting report request {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+async def update_poke_request(poke_request: PokeRequest) -> dict:
+    try:
+        query = "exec pokequeue.update_poke_request ?, ?, ?"
+        if not poke_request.url:
+            poke_request.url = "";
+        
+        params = (poke_request.id, poke_request.status, poke_request.url)
+        result = await execute_query_json(query, params, True)
+        result_dict = json.loads(result)
+        return result_dict
+    except Exception as e:
+        logger.error(f"Error updating report request {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
 async def insert_poke_request(poke_request: PokeRequest) -> dict:
     try:
         query = "exec pokequeue.create_poke_request ?"
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 import json
 from fastapi import FastAPI
 from utils.database import execute_query_json
-from controllers.PokeRequestController import insert_poke_request
+from controllers.PokeRequestController import insert_poke_request, update_poke_request, select_poke_request
 from models.PokeRequest import PokeRequest
 app = FastAPI()
 
@@ -17,9 +17,17 @@ async def root():
 async def version():
     return {"Version" : "0.1.0"}
 
+@app.get("/api/request/{id}")
+async def select_request(id : int):
+    return await select_poke_request(id)
+
 @app.post("/api/request")
 async def create_request(poke_request: PokeRequest):
     return await insert_poke_request(poke_request)
+
+@app.put("/api/request")
+async def update_request(poke_request: PokeRequest):
+    return await update_poke_request(poke_request)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
